14486be949cf22f5d7e750b366d90e4caaf2e877
Abner Rodrigues <abnerluis1001@gmail.com>
2022-10-23 16:50:22 -0300

    ✨ feat(eslint-config): add import/order rule
    BREAKING CHANGE: Add import/order rule. Any imports in the wrong order will error.

--- packages/eslint-config/rules/import.js
+++ packages/eslint-config/rules/import.js
@@ -3,7 +3,34 @@
 const ERROR = 2;
 
 module.exports = {
-  "import/first": ERROR,
-  "import/no-amd": ERROR,
-  "import/no-webpack-loader-syntax": ERROR,
+  'import/first': ERROR,
+  'import/no-amd': ERROR,
+  'import/no-webpack-loader-syntax': ERROR,
+  'import/order': [
+    ERROR,
+    {
+      'newlines-between': 'always',
+      groups: [
+        'builtin',
+        ['sibling, parent'],
+        'internal',
+        'external',
+        'type',
+        'object',
+        'index',
+      ],
+      pathGroups: [
+        {
+          pattern: '~/**',
+          group: 'external',
+          position: 'before',
+        },
+        {
+          pattern: '@/**',
+          group: 'external',
+          position: 'before',
+        },
+      ],
+    },
+  ],
 };
