1ba4234090ac2dbcc00ea953bf34c3dc68286527
Abner Rodrigues <abnerluis1001@gmail.com>
2022-10-23 16:48:48 -0300

    🔧 configuration(eslint-config): make ts rules stricter
    Make Typescript rules more stricter
    
    BREAKING CHANGE: Make Typescript rules stricter

--- packages/eslint-config/rules/ts.js
+++ packages/eslint-config/rules/ts.js
@@ -3,27 +3,27 @@ const WARN = 1;
 const ERROR = 2;
 
 module.exports = {
-  "no-dupe-class-members": OFF,
-  "no-undef": OFF,
+  'no-dupe-class-members': OFF,
+  'no-undef': OFF,
 
   // Add TypeScript specific rules (and turn off ESLint equivalents)
-  "@typescript-eslint/consistent-type-assertions": WARN,
-  "@typescript-eslint/consistent-type-imports": WARN,
+  '@typescript-eslint/consistent-type-assertions': ERROR,
+  '@typescript-eslint/consistent-type-imports': ERROR,
 
-  "no-array-constructor": OFF,
-  "@typescript-eslint/no-array-constructor": WARN,
+  'no-array-constructor': OFF,
+  '@typescript-eslint/no-array-constructor': ERROR,
 
   // There is a bug w/ @typescript-eslint/no-duplicate-imports triggered
   // by multiple imports inside of module declarations. We should reenable
   // this rule when the bug is fixed.
   // https://github.com/typescript-eslint/typescript-eslint/issues/3071
-  "no-duplicate-imports": OFF,
+  'no-duplicate-imports': OFF,
   // "@typescript-eslint/no-duplicate-imports": WARN,
 
-  "no-redeclare": OFF,
-  "@typescript-eslint/no-redeclare": ERROR,
-  "no-use-before-define": OFF,
-  "@typescript-eslint/no-use-before-define": [
+  'no-redeclare': OFF,
+  '@typescript-eslint/no-redeclare': ERROR,
+  'no-use-before-define': OFF,
+  '@typescript-eslint/no-use-before-define': [
     WARN,
     {
       functions: false,
@@ -32,45 +32,45 @@ module.exports = {
       typedefs: false,
     },
   ],
-  "no-unused-expressions": OFF,
-  "@typescript-eslint/no-unused-expressions": [
-    WARN,
+  'no-unused-expressions': OFF,
+  '@typescript-eslint/no-unused-expressions': [
+    ERROR,
     {
       allowShortCircuit: true,
       allowTernary: true,
       allowTaggedTemplates: true,
     },
   ],
-  "no-unused-vars": OFF,
-  "@typescript-eslint/no-unused-vars": [
+  'no-unused-vars': OFF,
+  '@typescript-eslint/no-unused-vars': [
     WARN,
     {
-      args: "none",
+      args: 'none',
       ignoreRestSiblings: true,
     },
   ],
-  "no-useless-constructor": OFF,
-  "@typescript-eslint/no-useless-constructor": WARN,
-  "class-methods-use-this": ERROR,
-  "eqeqeq": [ERROR, "always", { null: "ignore" }],
-  "no-alert": ERROR,
-  "no-console": ERROR,
-  "no-constructor-return": ERROR,
-  "no-lonely-if": ERROR,
-  "no-promise-executor-return": ERROR,
-  "no-shadow": ERROR,
-  "no-template-curly-in-string": ERROR,
-  "no-unreachable-loop": ERROR,
-  "no-warning-comments": [
+  'no-useless-constructor': OFF,
+  '@typescript-eslint/no-useless-constructor': ERROR,
+  'class-methods-use-this': ERROR,
+  eqeqeq: [ERROR, 'always', { null: 'ignore' }],
+  'no-alert': ERROR,
+  'no-console': ERROR,
+  'no-constructor-return': ERROR,
+  'no-lonely-if': ERROR,
+  'no-promise-executor-return': ERROR,
+  'no-shadow': ERROR,
+  'no-template-curly-in-string': ERROR,
+  'no-unreachable-loop': ERROR,
+  'no-warning-comments': [
     ERROR,
     {
-      terms: ["@nocommit"],
-      location: "anywhere",
+      terms: ['@nocommit'],
+      location: 'anywhere',
     },
   ],
-  "prefer-const": ERROR,
-  "require-await": ERROR,
-  "require-yield": ERROR,
-  "symbol-description": ERROR,
-  "yoda": ERROR,
+  'prefer-const': ERROR,
+  'require-await': ERROR,
+  'require-yield': ERROR,
+  'symbol-description': ERROR,
+  yoda: ERROR,
 };
