12020ce9468c7f4e465ec53d8f2f6cccc8cf5d1a
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-08 07:42:22 -0300

    refactor(eslint-config): create jsx config

---
+++ packages/eslint-config/jsx.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['./index.js', 'plugin:jsx-a11y/strict'],
+  plugins: ['import', 'jsx-a11y'],
+};
--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -2,22 +2,17 @@ const reactRules = require('./rules/react');
 const reactSettings = require('./settings/react');
 
 module.exports = {
-  extends: [
-    './index.js',
-    "plugin:react/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:jsx-a11y/strict",
-  ],
-  plugins: ["import", "react", "react-hooks", "jsx-a11y"],
+  extends: ['./index.js', './jsx.js', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  plugins: ['react', 'react-hooks'],
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("@babel/preset-react")],
+      presets: [require.resolve('@babel/preset-react')],
     },
   },
   settings: {
     ...reactSettings,
   },
   rules: {
-    ...reactRules
-  }
+    ...reactRules,
+  },
 };
