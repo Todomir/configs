6e48761a2674d56bafcf3bb07976f1e8fd01aaa0
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-11 07:03:51 -0300

    chore: update import sorting rules

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -20,24 +20,16 @@ module.exports = {
   singleAttributePerLine: false,
   vueIndentScriptAndStyle: false,
 
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-curly",
+    "prettier-plugin-jsdoc",
+    "prettier-plugin-packagejson",
+  ],
+
   // Import sorting rules
   importOrder: [
-    // Vue, React, Svelte, Qwik, Astro, Nuxt, Next, etc.
-    "^react$", // React
-    "^react-dom$", // React
-    "^svelte$", // Svelte
-    "^vue$", // Vue
-    "^astro$", // Astro
-    "^@builder-io/qwik$", // Qwik
-    "^next$", // Next
-    "^nuxt$", // Nuxt
-    "",
-    "^@core/(.*)$",
-    "^@server/(.*)$",
-    "^@client/(.*)$",
-    "^@ui/(.*)$",
-    "^@utils/(.*)$",
-    "^@types/(.*)$",
+    "^(react|next|solid|astro|@builder-io|vue|nuxt|angular|svelte)$",
     "",
     "<BUILTIN_MODULES>",
     "",
@@ -46,23 +38,13 @@ module.exports = {
     "",
     "<THIRD_PARTY_MODULES>",
     "",
-    "^(@/)(~/)(/.*)", // Absolute imports
-    "^~/(.*)$", // Tilde imports
+    "^(@api|@assets|@ui|@|~)(/.*)$",
     "",
-    "^[^./]", // Anything that does not start with a dot
-    "^[./]", // Anything that starts with a dot
+    "^[.]",
     "",
-    "^(?!.*\\.css$).*\\./.*$",
-    ".css$", // CSS imports
+    "^(?!.*[.]css$)[./].*$",
+    ".css$",
   ],
-  // Since prettier 3.0, manually specifying plugins is required
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-curly",
-    "prettier-plugin-jsdoc",
-    "prettier-plugin-packagejson",
-  ],
-  // This plugin's options
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderTypeScriptVersion: "5.0.0",
 };
